6fee6ac4f8623076f1a45ec41b20bf68a3fbccd1
ajayyedge <ajayyedge764@gmail.com>
2024-02-06 17:47:38 +0530

    final

---
+++ PORTFOLIO/script.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector(".hamburger");
+        hamburger.addEventListener("click", function () {
+            document.querySelector(".portfolio-navbar").classList.toggle("show");
+        });
